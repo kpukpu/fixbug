c6c6b6436a8a17688bba5422bf5d5f3edf0ed206
kimseungju <daejo99@naver.com>
2024-11-18 18:40:56 +0900

    z

---
+++ Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <Footer className="Footer">
+            <div className="logo">FIX BUG</div>
+            <nav>
+                <ul className="nav-list">
+                    <li>contact 알파실 </li>
+                </ul>
+            </nav>
+        </Footer>
+    );
+};
+
+export default Footer;
